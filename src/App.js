bf2c554910de4094b846909b9c55689cd47664d1
georgeampadu <george13@validsprep.org>
2021-03-22 14:41:40 -0400

    check out my github page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my Github Acct!!
         </a>
       </header>
     </div>
